cb01045c98d606ce19ef9a8b84be899923e62ad7
punit-0522 <punitjaiswal785621@gmail.com>
2026-04-20 01:29:39 +0530

    added submit and exam result routes

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 urlpatterns = [
     path('course/<int:course_id>/', views.course_detail, name='course_detail'),
+
+    # REQUIRED: submit route
+    path('course/<int:course_id>/submit/', views.submit, name='submit'),
+
+    # REQUIRED: exam result route
+    path(
+        'course/<int:course_id>/submission/<int:submission_id>/result/',
+        views.show_exam_result,
+        name='show_exam_result'
+    ),
 ]
