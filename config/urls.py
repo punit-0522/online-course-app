a3b7a7826abf276598eb24cceecf970dc7bf638b
punit-0522 <punitjaiswal785621@gmail.com>
2026-04-20 01:04:46 +0530

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Course App Running")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('', include('courses.urls')),
 ]
