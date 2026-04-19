0791931ea8cc8ee7e628ddbd716ad14a7b77f0d9
punit-0522 <punitjaiswal785621@gmail.com>
2026-04-20 01:38:40 +0530

    Update models.py

--- courses/models.py
+++ courses/models.py
@@ -1,33 +1,35 @@
 from django.db import models
 
-# Existing Course model (if not present, create it)
+
 class Course(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    title = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
-# Question model
-class Question(models.Model):
+class Lesson(models.Model):
+    title = models.CharField(max_length=200)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    question_text = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.question_text
+        return self.title
 
 
-# Choice model
+class Question(models.Model):
+    text = models.CharField(max_length=300)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    text = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.choice_text
 
-
-# Submission model
 class Submission(models.Model):
-    choices = models.ManyToManyField(Choice)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
