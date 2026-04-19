31473095492d81fb892d764e0b0edbb1ef3c0e5a
punit-0522 <punitjaiswal785621@gmail.com>
2026-04-20 01:17:47 +0530

    added required admin inline classes for assignment

--- courses/admin.py
+++ courses/admin.py
@@ -2,31 +2,26 @@ from django.contrib import admin
 from .models import Course, Lesson, Question, Choice, Submission
 
 
-# Inline for Choice inside Question
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 
-# Inline for Question inside Lesson
 class QuestionInline(admin.TabularInline):
     model = Question
     extra = 1
 
 
-# Admin for Question
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ['text']
 
 
-# Admin for Lesson
 class LessonAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
     list_display = ['title', 'course']
 
 
-# Register models with proper admin
 admin.site.register(Course)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Question, QuestionAdmin)
